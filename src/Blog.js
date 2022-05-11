b0f3d26529f2e65ce95d1edbf3ab9f5c799f1f52
psoltis7 <padgy24@yahoo.com>
2022-05-10 21:59:17 -0400

    commit

--- src/Blog.js
+++ src/Blog.js
@@ -1,3 +1,5 @@
+import * as React from "react";
+
 const Blog = () => {
     return (
         <div className="blog">
--- src/Projects.js
+++ src/Projects.js
@@ -1,4 +1,4 @@
-import React from "react";
+import * as React from 'react';
 import Coming from './images/coming-soon.jpg';
 
 const Projects = () => {
