6f47659529deba38d48140942a452902654dd6d9
psoltis7 <padgy24@yahoo.com>
2022-05-10 21:26:18 -0400

    commit

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,12 +1,14 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
     return (
         <nav className="navbar">
             <h1>Padgy Soltis</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/blog">Blog</a>
-                <a href="/resume">Resume</a>
-                <a href="/projects">Projects</a>
+                <Link to="/">Home</Link>
+                <Link to="/blog">Blog</Link>
+                <Link to="/resume">Resume</Link>
+                <Link to="/projects">Projects</Link>
             </div>
         </nav>
     );
