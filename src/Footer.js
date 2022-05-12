c2140b0ac8359975eb16e1bb96544c98939ffb89
psoltis7 <padgy24@yahoo.com>
2022-05-12 14:24:41 -0400

    update

--- src/Footer.js
+++ src/Footer.js
@@ -9,15 +9,15 @@ const Footer = () => {
     return (
         <footer>
              <div className="footer">
-                <a href="https://twitter.com/PadgySoltis"
+                <a href="https://twitter.com/PadgySoltis" target="_blank" rel="noreferrer noopener"
                 className="twitter">
                 <FontAwesomeIcon icon={faTwitter} size="2x" />
                 </a>
-                <a href="https://github.com/psoltis7"
+                <a href="https://github.com/psoltis7" target="_blank" rel="noreferrer noopener"
                 className="github">
                 <FontAwesomeIcon icon={faGithub} size="2x" /> 
                 </a>
-                <a href="https://www.linkedin.com/in/padgy-soltis"
+                <a href="https://www.linkedin.com/in/padgy-soltis" target="_blank" rel="noreferrer noopener"
                 className="linkedin">
                 <FontAwesomeIcon icon={faLinkedin} size="2x" />
                 </a>
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import * as ReactDOM from "react-dom";
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from "react-router-dom";
 import './index.css';
